c6b3a242f03ec8fda5a8342c225dd0251a6a1695
Callum Nelson <callum.nelson95@gmail.com>
2023-04-16 15:16:29 -0400

    stub up routes and style new flight form

--- routes/flights.js
+++ routes/flights.js
@@ -4,9 +4,19 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
-// GET localhost:3000/users
-router.get('/', function(req, res) {
-  res.render('flights/index')
-})
+// GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
+
+// POST localhost:3000/flights
+router.post('/', flightsCtrl.create)
+
+// GET localhost:3000/flights/new
+router.get('/new', flightsCtrl.new)
+
+// GET localhost:3000/flights/:flightId
+router.get('/:flightId', flightsCtrl.show)
+
+// DELETE localhost:3000/flights/:flightId
+router.delete('/:flightId', flightsCtrl.delete)
 
 export { router }
