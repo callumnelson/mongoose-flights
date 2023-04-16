5ebe560510a0cb1596823e88fbb20e042ce460ce
Callum Nelson <callum.nelson95@gmail.com>
2023-04-16 15:45:45 -0400

    create new flight and index pages and controllers

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,17 @@
 import { Flight } from "../models/flight.js"
 
 const index = async (req, res) => {
-
+  try {
+    const flights = await Flight.find({})
+    console.log(flights)
+    res.render('flights/index', {
+      title: 'All Flights',
+      flights
+    })
+  }catch(err){
+    console.log(err)
+    res.redirect('/')
+  }
 }
 
 const newFlight = async (req, res) => {
@@ -17,13 +27,12 @@ const create = async (req, res) => {
       console.log(key, req.body[key])
       if (req.body[key] === '') delete req.body[key]
     }
-    console.log()
+    await Flight.create(req.body)
   }catch(err){
     console.log(err)
   } finally {
-    res.redirect('/flights')
+    res.redirect('/flights/new')
   }
-}
 }
 
 const deleteFlight = async (req, res) => {
