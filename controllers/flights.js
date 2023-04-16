d812e4d79e5b3de5424c3d32ce500a671ea501aa
Callum Nelson <callum.nelson95@gmail.com>
2023-04-16 15:06:29 -0400

    stub up controllers and views, style icon

---
+++ controllers/flights.js
@@ -0,0 +1,29 @@
+import { Flight } from "../models/flight.js"
+
+const index = async (req, res) => {
+
+}
+
+const newFlight = async (req, res) => {
+
+}
+
+const create = async (req, res) => {
+
+}
+
+const deleteFlight = async (req, res) => {
+
+}
+
+const show = async (req, res) => {
+
+}
+
+export {
+  index,
+  newFlight as new,
+  create,
+  deleteFlight as delete,
+  show
+}
